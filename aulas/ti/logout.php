f7c6a86a48c3a256d1a17db870117e793e094087
André Costa <costandre01@gmail.com>
2026-05-09 13:42:54 +0100

    Remove unnecessary system files

--- aulas/ti/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();   //diz ao browser para utilizar variáveis de sessão;
-
-session_unset();   // remove todas as variáveis de sessão;
-
-session_destroy();  // destrói a sessão;
-header( "refresh:0;url=index.php" );  
-?>
--- aulas/ti/python/olamundo.py
+++
@@ -1,4 +0,0 @@
-import time
-
-print("Ola Mundo")
-print(time.strftime("%Y-%m-%d %H:%M:%S"))
